3b2d25937b0d1132a5d8cddb3b6a741eb2eb64ba
JavierGoto02 <gotojavier9@gmail.com>
2025-01-28 18:53:38 -0300

    Primer commit del branch prueba

--- piton.py
+++ piton.py
@@ -4,4 +4,4 @@ numero += 11
 print("Hola como estas, esta linea de codigo todavia no se hizo commit, modificando...")
 print("Esta linea no esta todavia en el stagin area")
 print("Modificando desde github")
-
+print("Linea agregada desde el branch prueba")
