8d6f4011cfc088010c9b304b60af5f99228422ca
Javier Goto <132099390+JavierGoto02@users.noreply.github.com>
2025-01-28 16:24:40 -0300

    Update piton.py

--- piton.py
+++ piton.py
@@ -3,4 +3,5 @@ numero = 89
 numero += 11
 print("Hola como estas, esta linea de codigo todavia no se hizo commit, modificando...")
 print("Esta linea no esta todavia en el stagin area")
+print("Modificando desde github")
 
