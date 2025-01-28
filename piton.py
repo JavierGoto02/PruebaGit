eaa690b876734e5e5dfd52b39d08d524e037e859
JavierGoto02 <gotojavier9@gmail.com>
2025-01-28 12:05:12 -0300

    TercerCommit

--- piton.py
+++ piton.py
@@ -1,4 +1,6 @@
 print("Imprimiendo...")
 numero = 89
 numero += 11
+print("Hola como estas, esta linea de codigo todavia no se hizo commit, modificando...")
+print("Esta linea no esta todavia en el stagin area")
 
